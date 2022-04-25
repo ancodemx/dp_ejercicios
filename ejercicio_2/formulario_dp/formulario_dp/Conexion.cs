dbd301cae3edd51b20c752f029110340c51e683c
ANcodeMX <ancodemx@gmail.com>
2022-04-24 18:09:03 -0600

    Agregamos la Clase Conexion

---
+++ ejercicio_2/formulario_dp/formulario_dp/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace formulario_dp
+{
+    internal class Conexion
+    {
+
+        public static MySqlConnection conexion()
+        {
+            string servidor = "localhost";
+            string bd = "practica_dp";
+            string usuario = "root";
+            string password = "";
+
+            string cadenaConexion = "Database="+bd+"; Data Source="+servidor+"; User Id="+usuario+"; Password="+password+"";
+
+            try {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch(MySqlException ex) {
+                Console.WriteLine("Error: " + ex.Message);
+
+                return null;
+            }
+        }
+
+    }
+}
